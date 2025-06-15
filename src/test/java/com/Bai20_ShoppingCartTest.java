a9adafa1807aa32a64a2c008dabc951a31b3146c
Le Ho <leho.tdc2019@gmail.com>
2025-06-15 23:39:53 +0700

    remove library

--- src/test/java/com/Bai20_ShoppingCartTest.java
+++ src/test/java/com/Bai20_ShoppingCartTest.java
@@ -1,16 +1,13 @@
 package com;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
-import com.utils.Utils;
 
 public class Bai20_ShoppingCartTest extends BasicTest{
     // @Test
