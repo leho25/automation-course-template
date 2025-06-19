d7947a73b3ecc42442309f808c289fbaf3fa760a
Le Ho <leho.tdc2019@gmail.com>
2025-06-19 18:34:06 +0700

    implement the POM

---
+++ src/main/java/com/config/Constains.java
@@ -0,0 +1,8 @@
+package com.config;
+
+public class Constains {
+    public static final String BASE_URL = "https://bantheme.xyz/hathanhauto";
+    public static final String URL = BASE_URL + "/tai-khoan/";
+    public static final String HOMEPAGE_URL = BASE_URL + "/";
+
+}
---
+++ src/main/java/com/pages/HomeDetailPage.java
@@ -0,0 +1,23 @@
+package com.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomeDetailPage extends BasePage {
+    public HomeDetailPage(WebDriver driver) {
+        super(driver);
+    }
+
+    By clickAddCartButton = By.xpath("//button[contains(@class,'single_add_to_cart_button')]");
+
+    public void selectOptionDropDown(String selectOption) {
+        By selectValue = By.xpath("//option[contains(text(),'" + selectOption + "')]");
+        fidElement(selectValue).click();
+    }
+
+    public HomeDetailPage clickAddCart() {
+        clickElement(clickAddCartButton);
+        return this;
+    }
+
+}
