49fc29c60881a469bc123b892411fd3f1c72df56
Le Ho <leho.tdc2019@gmail.com>
2025-06-22 15:09:58 +0700

    add Test Bai 20

--- src/test/java/com/Bai20_ShoppingCartTest.java
+++ src/test/java/com/Bai20_ShoppingCartTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import com.utils.BasicTest;
 
